7ffd8bd0574be49adc13d1c1aec7239357e72122
gustavo litter <gustavolitter@gmail.com>
2022-08-18 00:13:48 -0300

    Criado novo middleware para encontrar o repository e trazer os dados.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ app.use(express.json());
 
 const repositories = [];
 
+function findRepository(request, response, next) {
+  const { id } = request.params;
+  const repository = repositories.find(repository => repository.id === id);
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+  request.repository = repository;
+  return next();
+};
+
 function findRepositoryIndex(request, response, next) {
   const { id } = request.params;
   const repository = repositories.findIndex(repository => repository.id === id);
@@ -13,7 +23,7 @@ function findRepositoryIndex(request, response, next) {
   }
   request.repository = repository;
   return next();
-}
+};
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -32,17 +42,12 @@ app.post("/repositories", (request, response) => {
   return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+app.put("/repositories/:id", findRepository, (request, response) => {
   const { title, url, techs } = request.body;
-  const repository = repositories.find(repository => repository.id === id);
-  if (!repository) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-  repository.title = title;
-  repository.url = url;
-  repository.techs = techs;
-  return response.status(201).json(repository);
+  request.repository.title = title;
+  request.repository.url = url;
+  request.repository.techs = techs;
+  return response.status(201).json(request.repository);
 });
 
 app.delete("/repositories/:id", findRepositoryIndex, (request, response) => {
@@ -50,7 +55,7 @@ app.delete("/repositories/:id", findRepositoryIndex, (request, response) => {
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", findRepositoryIndex, (request, response) => {
+app.post("/repositories/:id/like", findRepository, (request, response) => {
   const likes = request.repository.likes++;
   return response.json(likes);
 });
